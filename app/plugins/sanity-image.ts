ddfb4d3725ba7f17e55c15b11b63ef16eccd6b3d
Shaun Berkley <shaun@berkleysoftware.com>
2024-06-03 15:35:08 -0700

    Update sanity image builder setup

--- app/plugins/sanity-image.ts
+++ app/plugins/sanity-image.ts
@@ -2,8 +2,10 @@ import imageUrlBuilder from '@sanity/image-url'
 import type { Image } from '@sanity/types'
 
 export default defineNuxtPlugin(() => {
-  const sanity = useSanity()
-  const builder = imageUrlBuilder(sanity.client)
+  const builder = imageUrlBuilder({
+    projectId: process.env.NUXT_SANITY_PROJECT_ID ?? '',
+    dataset: 'production',
+  })
 
   function urlFor(source: Image) {
     return builder.image(source)
